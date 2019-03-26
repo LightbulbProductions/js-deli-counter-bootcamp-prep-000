6daa7bf55a2947899e6fd04986889d9a33485d1e
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 14:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine=[];                  
 
-function takeANumber(katzDeliLine, name = "Guest")
+function takeANumber(katzDeliLine, name = "Guest"){
     katzDeliLine.push(name)
     console.log(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
     return `Welcome, ${name} .  You are number ${katzDeliLine.length} in line.`;
