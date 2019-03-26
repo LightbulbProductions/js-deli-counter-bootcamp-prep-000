632d5499b1fc4170c41b720fb0ac96efdda9d284
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 14:37:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 var katzDeliLine=[];                   //Current Guests already waiting in line
 
 function takeANumber(katzDeliLine,name = "Guest"){            //
-    katzDeliLine.push(name)
-    console.log(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
-    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+    katzDeliLine.push(name);
+    console.log(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(line){
- if( line.length===0){
-   console.log("There is nobody waiting to be served!")
-   return "There is nobody waiting to be served!"
+ if( line.length === 0){
+   console.log("There is nobody waiting to be served!");
+   return "There is nobody waiting to be served!";
   }else{
-    console.log(`Currently serving ${line.shift()}.`)
-    //return `Currently serving ${line.shift()}.`
+    console.log(`Currently serving ${line.shift()}.`);
    }
 }
 
 function currentLine(line){
-  if(line.length===0){
-    console.log("Line is currently empty")
-    //return "The line is currently empty."
+  if(line.length === 0){
+    console.log("Line is currently empty");
   }else{
     var newLine = [],i;
-    //line.forEach(function(item,index){newLine.push(` ${index+1}. ${item}`)} )
-    for(i=0; i<line.length;i++){
-      newLine.push(` ${i+1}. ${line[i]}`)
+    for(i=0; i < line.length; i++){
+      newLine.push(` ${i+1}. ${line[i]}`);
     }
-    console.log(`The line is currently:${newLine}`)
-    return `The line is currently:${newLine}`
+    console.log(`The line is currently:${newLine}`);
+    return `The line is currently:${newLine}`;
   }
 }
 
