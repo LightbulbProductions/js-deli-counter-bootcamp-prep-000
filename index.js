968d24968ed0691bfdd1820e2adf3e70938cfadb
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 14:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(katzdeli){
     i++;
   }
  if( katzDeli.length === 0){
-   else
+
    return "There is nobody waiting to be served!";
   }else{
     console.log(`Currently serving ${katzDeli.shift()}.`);
